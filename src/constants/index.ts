cf38739b47896c8f69b9666123765f2e4b8391ea
coding946 <chenkang84@gmail.com>
2024-12-13 11:00:31 +0800

    feature: add thrower game

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -119,6 +119,15 @@ export const GAME_TEST_LIST = [
       groups: 0,
       rounds: 0,
     },
+    {
+      game_id: 13,
+      game_short_name: 'LuckyThrower',
+      game_show_name: 'Lucky Thrower',
+      game_url: 'https://t.me/myweb3gamebot/LuckyThrower',
+      game_img: new URL('@/assets/images/game_thrower.png', import.meta.url).href,
+      groups: 0,
+      rounds: 0,
+    },
 ]
 export const GAME_PROD_LIST = [
     {
@@ -226,6 +235,15 @@ export const GAME_PROD_LIST = [
       game_show_name: 'Lucky Crosser',
       game_url: 'https://t.me/LuckyMiuuu_bot/LuckyCrosser',
       game_img: new URL('@/assets/images/game_crosser.png', import.meta.url).href,
+      groups: 0,
+      rounds: 0,
+    },
+    {
+      game_id: 13,
+      game_short_name: 'LuckyThrower',
+      game_show_name: 'Lucky Thrower',
+      game_url: 'https://t.me/LuckyMiuuu_bot/LuckyThrower',
+      game_img: new URL('@/assets/images/game_thrower.png', import.meta.url).href,
       groups: 0,
       rounds: 0,
     },
