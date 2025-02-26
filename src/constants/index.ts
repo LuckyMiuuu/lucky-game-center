a90587f48d258291471d28422aac06a400c1525f
coding946 <chenkang84@gmail.com>
2025-02-26 12:19:22 +0800

    add game lumberjack

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -209,6 +209,15 @@ export const GAME_TEST_LIST = [
       groups: 0,
       rounds: 0,
     },
+    {
+      game_id: 23,
+      game_short_name: 'LuckyLumberjack',
+      game_show_name: 'Lucky Lumberjack',
+      game_url: 'https://t.me/myweb3gamebot/LuckyLumberjack',
+      game_img: new URL('@/assets/images/game_lumberjack.png', import.meta.url).href,
+      groups: 0,
+      rounds: 0,
+    },
 ]
 export const GAME_PROD_LIST = [
     {
@@ -406,6 +415,15 @@ export const GAME_PROD_LIST = [
       game_show_name: 'Lucky Plumber',
       game_url: 'https://t.me/LuckyMiuuu_bot/LuckyPlumber',
       game_img: new URL('@/assets/images/game_plumber.png', import.meta.url).href,
+      groups: 0,
+      rounds: 0,
+    },
+    {
+      game_id: 23,
+      game_short_name: 'LuckyLumberjack',
+      game_show_name: 'Lucky Lumberjack',
+      game_url: 'https://t.me/LuckyMiuuu_bot/LuckyLumberjack',
+      game_img: new URL('@/assets/images/game_lumberjack.png', import.meta.url).href,
       groups: 0,
       rounds: 0,
     },
